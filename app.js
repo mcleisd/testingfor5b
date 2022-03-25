0bd48e2508c585e783d52c68d2cc75944f1649dc
Manuel Castillo <mcastillo@littleelmisd.net>
2022-03-25 11:51:36 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+// 11:22 -- 11:28
+
+function human() { // 10
+    var n = prompt("What's your name?")
+    return n
+}
+
+function temp() { // 20
+    var t = prompt("What's the temperature?")
+    return t
+}
+
+function cToF(x) { // 20
+    var cf = ((x * 9) / 5) + 32
+    return cf
+}
+
+function fToC(y) { //20
+    var fc = ((y - 32) * 5) / 9
+    return fc
+
+
+function conversion() {
+    var c = prompt("Do you want to convert from F to C -press 1-, or from C to F --press 2")
+    return c
+}
